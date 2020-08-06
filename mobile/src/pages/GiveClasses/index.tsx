c97e140da1616c002ec16ff8cf2c7d85ae7e2d57
Luiz.Uliana <luizeduardr@gmail.com>
2020-08-06 20:45:33 -0300

    Done screens to study and teach classes, stylization of the page header, stack navigation and tab navigation settings.

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import bgImage from '../../assets/images/give-classes-background.png';
+
+
+const GiveClasses = () => {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack(){
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={bgImage}
+        style={styles.content}
+        resizeMode="contain">
+        <Text style={styles.title}>Quer ser um proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>
+          Tudo Bem
+        </Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
